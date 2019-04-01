5f4860c086b6ccbb3173df03b8ca927ee686827c
JamesBerry <a9883805@9883805s-MacBook-Pro.local>
2019-04-01 15:24:50 +0100

    notes added

--- hello_world.js
+++ hello_world.js
@@ -4,3 +4,8 @@
 // ➜  hello_world atom .
 // ➜  hello_world
 console.log("Hello World!");
+// ➜  hello_world git init
+// Initialized empty Git repository in /Users/a9883805/Desktop/codeclan_work/week_01/day_01/hello_world/.git/
+// ➜  hello_world git:(master) ✗ git add .
+// ➜  hello_world git:(master) ✗ git commit -m "homework completed"
+// ➜  hello_world git:(master) git push
